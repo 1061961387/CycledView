5948aa3263324bbf4ae6f3e7b969a920e9d9b88e
wangnan <1061961387@qq.com>
2015-06-29 16:48:53 +0800

    Revert "Revert "Revert "Revert "Revert "Revert "Initial Commit""""""
    This reverts commit d2cb85bc57e126500a22041df1040a629b68fe78.

---
+++ CycledViewTests/CycledViewTests.m
@@ -0,0 +1,40 @@
+//
+//  CycledViewTests.m
+//  CycledViewTests
+//
+//  Created by 裕福 on 15/6/29.
+//  Copyright (c) 2015年 裕福. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CycledViewTests : XCTestCase
+
+@end
+
+@implementation CycledViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
